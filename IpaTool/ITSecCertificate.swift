ce15ea7bd3dbbecdc23ebcf62bfaed08cb1b3ccd
Stefan van den Oord <soord@mac.com>
2014-11-24 17:50:23 +0100

    Simplified some statements

--- IpaTool/ITSecCertificate.swift
+++ IpaTool/ITSecCertificate.swift
@@ -17,44 +17,26 @@ class ITSecCertificate
     }
     
     init(_ data:NSData) {
-        var uCert:Unmanaged<SecCertificate> = SecCertificateCreateWithData(nil, data)
-        _secCertificate = uCert.takeRetainedValue()
+        _secCertificate = SecCertificateCreateWithData(nil, data).takeRetainedValue()
     }
     
     var commonName : String? {
         get {
             var cn:Unmanaged<CFString>? = nil
             SecCertificateCopyCommonName(_secCertificate, &cn)
-            if let c = cn {
-                return c.takeRetainedValue()
-            }
-            else {
-                return nil
-            }
+            return cn?.takeRetainedValue()
         }
     }
     
     var subject : String? {
         get {
-            var subject:Unmanaged<CFString>? = SecCertificateCopySubjectSummary(_secCertificate)
-            if let s = subject {
-                return s.takeRetainedValue()
-            }
-            else {
-                return nil
-            }
+            return SecCertificateCopySubjectSummary(_secCertificate).takeRetainedValue()
         }
     }
     
     var values : NSDictionary? {
         get {
-            var certValues = SecCertificateCopyValues(_secCertificate, nil, nil)
-            if let c = certValues {
-                return c.takeRetainedValue()
-            }
-            else {
-                return nil
-            }
+            return SecCertificateCopyValues(_secCertificate, nil, nil).takeRetainedValue()
         }
     }
 }
